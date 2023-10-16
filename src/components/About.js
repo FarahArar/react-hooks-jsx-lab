4d338cb19114b840a6a850373767a7aef78242c4
Farah Arar <farah02arar@gmail.com>
2023-10-16 13:58:29 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,16 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return
+   <div id="about">
+    About
+    <h1 style={{ color: "firebrick" }}>
+    {('{name} is a Web Developer from {city}')}
+    </h1>
+    <h2>About Me</h2>
+    <p>Some content of my choosing.</p>
+    <img src={image} alt="I made this" />
+    </div>;
 }
 
 export default About;
